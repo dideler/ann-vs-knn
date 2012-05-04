a7d6a9041a7385fbf7a39e67f2db4d0e7ec1a66c
Dennis Ideler <ideler.dennis@gmail.com>
2012-05-04 19:42:57 -0400

    Make some functions const. Change classification code.

--- src/NearestNeighbour.cpp
+++ src/NearestNeighbour.cpp
@@ -31,7 +31,7 @@ NearestNeighbour::~NearestNeighbour()
  * Finds the k closest instances and takes the majority class.
  */
 void NearestNeighbour::learn(const vector< vector<float> > training_set,
-                             const vector< vector<float> > testing_set)
+                             const vector< vector<float> > testing_set) const
 {
   // For each unclassified example (ie instance in testing set),                     
   int total_hits = 0;
@@ -39,18 +39,7 @@ void NearestNeighbour::learn(const vector< vector<float> > training_set,
   for (int i = 0; i < total_cases; ++i)
   {
     int classification = computeNearestNeighbours(testing_set[i], training_set);
-    
-    int target = testing_set[i][size];
-#if 0
-    for (int j = 0; j < num_attributes_; ++j)
-    {
-      if (testing_set[i][size + i] == 1)
-      {
-        target = i + 1;
-        break;
-      }
-    }
-#endif
+    int target = testing_set[i][num_attributes_];
 
     if (classification == target) ++total_hits;
     cout << "Expected outcome: " << target << "\n" // TODO: remove after testing
@@ -70,31 +59,22 @@ void NearestNeighbour::learn(const vector< vector<float> > training_set,
  */
 int NearestNeighbour::computeNearestNeighbours(
     const vector<float> query,
-    const vector< vector<float> > training_set)
+    const vector< vector<float> > training_set) const
 {
   int size = training_set.size();
   vector<neighbour> neighbours;
   for (int i = 0; i < size; ++i)
   {
-    neighour n;
-    // Get distance.
+    neighbour n;
+    n.distance = dist(query, training_set[i]); // TODO: Also try distR.
     // TODO: weighted knn
     // If weighted, multiply each examples vote (i.e. distance) by a weight.           
     // weight_i = 1 / dist(query, record_i)^2
-    n.distance = dist(query, training_set[i]); // TODO: Also try distR.
-    // Get classification of current labeled example.
-    for (int j = 0; j < num_attributes_; ++j) // TODO: function for getting the classification or make it the last item in vector.
-    {
-      if (training_set[i][num_attributes_ + i] == 1)
-      {
-        n.classification = i + 1;
-        break;
-      }
-    }
+    n.classification = training_set[i][num_attributes_];
     neighbours.push_back(n);
   }
 
-  sort(neighbours.begin(), neighbours.end(), compareDist);
+  sort(neighbours.begin(), neighbours.end());
  
   // Tally up the votes from the k nearest neighbours.
   map<int, int> votes;
@@ -130,7 +110,7 @@ int NearestNeighbour::computeNearestNeighbours(
  * @return The distance between the two examples.
  */
 double NearestNeighbour::dist(const vector<float> query,
-                              const vector<float> record)
+                              const vector<float> record) const
 {
   assert(query.size() == record.size());
   double temp = 0;
@@ -150,7 +130,7 @@ double NearestNeighbour::dist(const vector<float> query,
  * @return The squared "distance" between the two examples.
  */
 double NearestNeighbour::distR(const vector<float> query,
-                               const vector<float> record)
+                               const vector<float> record) const
 {
   assert(query.size() == record.size());
   double temp = 0;
@@ -164,15 +144,7 @@ double NearestNeighbour::distR(const vector<float> query,
 /**
  * A generic function to square a value.
  */
-template<class T> inline T NearestNeighbour::sqr(const T &x)
+template<class T> inline T NearestNeighbour::sqr(const T &x) const
 {
   return x * x;
-}
-
-/**
- * Comparison function for sort.
- */
-bool NearestNeighbour::compareDist(const neighbour &a, const neighbour &b)
-{
-  return a.distance < b.distance;
 }
