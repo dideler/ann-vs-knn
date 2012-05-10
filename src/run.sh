8aa505716f21289f770f066d145ae4cc2633888c
Dennis Ideler <ideler.dennis@gmail.com>
2012-05-10 17:32:27 -0400

    Clean up single run script

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,7 @@
 
 # (C) 2012 Dennis Ideler
 #
+# Performs a single run on the ANN and K-NN.
 # Example:
 #   $ bash run.sh 1 75 ann-err.out ann-acc.out knn-acc.out
 #
@@ -10,34 +11,13 @@
 set -e # Error checking.
 set -u # Parameter checking.
 
-SEED="$1"
-RATIO="$2"
-ERROR="$3"
-ACCURACY="$4"
-KNN_ACCURACY="$5"
+CONF="$1"
+DATA="$2"
+SEED="$3"
+RATIO="$4"
+ANN_ERROR="$5"
+ANN_ACCURACY="$6"
+KNN_ACCURACY="$7"
 
-echo "Random number generator seed = $SEED"
-echo "Training samples percentage = $RATIO"
-echo "Output filename for network error = $ERROR"
-echo "Output filename for network accuracy = $ACCURACY"
-echo "Output filename for k-NN accuracy = $KNN_ACCURACY"
-read -p "Are these values correct? (y/n) " -n 1
-echo
-echo
-
-function run()
-{
-  # Experiment 1 (steel dataset)
-  # For verbose output, use the -v tag (warning: running time will be extra long).
-  ./ann-vs-knn -c steel.conf -d ./data/faults-simple.data -s $SEED -t $RATIO -e $ERROR -a $ACCURACY -k $KNN_ACCURACY -p
-  ./ann-vs-knn -c steel-subset.conf -d ./data/faults-subset-simple.data -s $SEED -t $RATIO -e $ERROR -a $ACCURACY -k $KNN_ACCURACY -p
-
-  # Experiment 2 (digit dataset)
-  #./ann-vs-knn -c digits.conf -d ./data/digits-simple.data -s $SEED -t $RATIO -e $ERROR -a $ACCURACY -p
-}
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  run
-fi
-
-unset run
+# For verbose output, use the -v tag (warning: running time will be extra long).
+./ann-vs-knn -c $CONF -d $DATA -s $SEED -t $RATIO -e $ANN_ERROR -a $ANN_ACCURACY -k $KNN_ACCURACY -p
