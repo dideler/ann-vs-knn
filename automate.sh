ff9f86f900885b2a803a2c77d8e18e35a7798b5a
Dennis Ideler <ideler.dennis@gmail.com>
2012-05-13 06:22:28 -0400

    Fix path issue. Add user feedback during execution.

--- automate.sh
+++ automate.sh
@@ -2,8 +2,23 @@
 
 set -e
 
-pushd > /dev/null src/
+echo
+echo "(!) Building project and cleaning up..."
+echo
+pushd src/  # To silence pushd and popd, redirect to /dev/null.
 make all-optimized
+echo
+echo "(!) Build complete."
+echo "(!) Starting experimental runs..."
+echo
 bash all_runs.sh
-popd > /dev/null
-bash /results/average-data.sh
+echo
+echo "(!) Runs complete."
+echo "(!) Processing results..."
+echo
+popd
+pushd results/
+bash average-data.sh
+popd
+echo
+echo "(!) All done!"
